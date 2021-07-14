7b61259db273fb71ed48144919a26050c120c841
jrod0975 <44900830+jrod0975@users.noreply.github.com>
2021-07-14 15:47:13 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+
+document.querySelector("h1").innerHTML = randomNumber1 + randomNumber2;
